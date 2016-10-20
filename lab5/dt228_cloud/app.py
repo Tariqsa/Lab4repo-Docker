5682baffcc44136af168e148d899954042bc2dfa
Tariqsa <D13125313@mydit.ie>
2016-10-20 03:37:54 +0300

    change

--- lab5/dt228_cloud/app.py
+++ lab5/dt228_cloud/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-if __name__ == "__main__":
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888,debug=True)
